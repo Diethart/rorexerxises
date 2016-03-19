1b9d2cda6112f64ac91f450094323aca165d84f6
Diethart Belgard <diethart19@gmail.com>
2016-03-19 18:41:25 +0300

    Сделал третье задание

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,10 @@
 class Card < ActiveRecord::Base
+	validates :original_text, presence: true
+	validates :translated_text, presence: true
+	validates :review_date, presence: true
+	validate :equal
+
+	def equal
+	  errors.add(:original_text, "can't be the same with translated_text") if original_text.downcase == translated_text.downcase
+	end
 end
